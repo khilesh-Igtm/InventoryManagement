7f688ff485ee21d6354e73b6e3040adb95d83be8
khilesh-Igtm <khileshsingh5678@gmail.com>
2025-08-10 12:02:53 +0530

    get req to backend

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+
+// Route imports
+import dashboardRoutes from "./routes/dashboardRoutes";
+
+
+// Configurations
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
+
+// Routes
+app.use("/dashboard",dashboardRoutes)
+
+
+// Server
+const port = process.env.PORT || 3001
+app.listen(port,()=>{
+  console.log(`Server is running at ${port}`)
+})
